761e3c909c1ca48c0b2c8dcb6e7cb8ffc6bebf50
Aaron Jones <ronj2005@gmail.com>
2021-10-07 00:15:04 -0400

    updated js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,43 +1,39 @@
-var myKey = "152ae2b8b820db732e591a903f32c1ef";     
-var apiURL = "https://api.openweathermap.org/data/2.5/weather?q="
-var ocApiURL ="https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=152ae2b8b820db732e591a903f32c1ef"
+var myKey = "152ae2b8b820db732e591a903f32c1ef";
+var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+var ocApiURL =
+  "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=152ae2b8b820db732e591a903f32c1ef";
 var bothAPI = apiURL + ocApiURL;
-// var input ="";     
+// var input ="";
 
 class Fetch {
-    async getCurrent(input) {
-  
-      //make request to url
-  
-      var response = await fetch(
-        
-        apiURL + input + "&appid=152ae2b8b820db732e591a903f32c1ef" //+ "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=152ae2b8b820db732e591a903f32c1ef"
-      );
-  
-      var data = await response.json();
-  
-      console.log(data);
-  
-      return data;
+  async getCurrent(input) {
+    //make request to url
 
-      
-    }
+    var response = await fetch(
+      apiURL + input + "&appid=152ae2b8b820db732e591a903f32c1ef" //+ "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=152ae2b8b820db732e591a903f32c1ef"
+    );
+    
+    var data = await response.json();
+
+    console.log(data);
+
+    return data;
+  }
+}
+
+class UI {
+  constructor() {
+    this.uiContainer = document.getElementById("content");
+    this.city;
+    this.defaultCity = "";
   }
 
-  class UI {
-    constructor() {
-      this.uiContainer = document.getElementById("content");
-      this.city;
-      this.defaultCity = "";
-    }
-  
-    populateUI(data) {
-      //de-structure vars
-  
-      //add them to inner HTML
-  
-      this.uiContainer.innerHTML = `
-          
+  populateUI(data) {
+    //de-structure vars
+
+    //add them to inner HTML
+
+    this.uiContainer.innerHTML = `     
           <div class="card mx-auto mt-5" style="width: 18rem;">
               <div class="card-body justify-content-center">
                   <h5 class="card-title">${data.name}</h5>
@@ -46,49 +42,43 @@ class Fetch {
                   
               </div>
           </div>
-          
-          
           `;
-    }
-  
-    clearUI() {
-      uiContainer.innerHTML = "";
-    }
-  
-    saveToLS(data) {
-      localStorage.setItem("city", JSON.stringify(data));
-    }
-  
-    getFromLS() {
-      if (localStorage.getItem("city" == null)) {
-        return this.defaultCity;
-      } else {
-        this.city = JSON.parse(localStorage.getItem("city"));
-      }
-  
-      return this.city;
-    }
-  
-    clearLS() {
-      localStorage.clear();
-    }
   }
-  
-  
 
+  clearUI() {
+    uiContainer.innerHTML = "";
+  }
 
+  saveToLS(data) {
+    localStorage.setItem("city", JSON.stringify(data));
+  }
 
-  //inst classes//
+  getFromLS() {
+    if (localStorage.getItem("city" == null)) {
+      return this.defaultCity;
+    } else {
+      this.city = JSON.parse(localStorage.getItem("city"));
+    }
 
-const ft = new Fetch();
-const ui = new UI();
+    return this.city;
+  }
+
+  clearLS() {
+    localStorage.clear();
+  }
+}
+
+//inst classes//
+
+var ft = new Fetch();
+var ui = new UI();
 
 //add event listeners//
 
-const search = document.getElementById("searchUser");
-const button = document.getElementById("submit");
+var search = document.getElementById("searchUser");
+var button = document.getElementById("submit");
 button.addEventListener("click", () => {
-  const currentVal = search.value;
+  var currentVal = search.value;
 
   ft.getCurrent(currentVal).then((data) => {
     //call a UI method//
@@ -101,53 +91,6 @@ button.addEventListener("click", () => {
 //event listener for local storage
 
 window.addEventListener("DOMContentLoaded", () => {
-  const dataSaved = ui.getFromLS();
+  var dataSaved = ui.getFromLS();
   ui.populateUI(dataSaved);
 });
-
-
-
-/////////////////////
-
-  // var response = await fetch(
-    
-  //   "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=152ae2b8b820db732e591a903f32c1ef"
-  // );
-
-  // var data = await response.json();
-
-  // console.log(data);
-
-  // return data;
-
-
-
-  ////////////////////
-//   function cityWeather () {
-//     var today = moment().format("MMM DD, YYYY");
-//     console.log(today);
-// }
-    
-
-// function getCurrent(input) {
-//     // cityName = inputTextForm.val();
-//     // console.log(inputTextForm.val());
-
-//     var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=152ae2b8b820db732e591a903f32c1ef"
-//     var ocApiURL ="https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=152ae2b8b820db732e591a903f32c1ef"
-
-//     fetch(apiURL) 
-//         .then(function(response) {
-//             return response.json();
-//         })
-//         .then(function(data) {
-//             console.log(data);
-        
-//     fetch(ocApiURL) 
-//         .then(function(response) {
-//             return response.json();
-//         })
-//         .then(function(data) {
-//             console.log(data);
-//         })
-//         }) 
